ed57ce124eb39beb2f9497f1afc2fdea4a04cbd5
Leonardo <leo_omegahot@hotmail.com>
2022-05-19 19:49:33 -0300

    User entity and recourse

---
+++ src/main/java/com/estudossts/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.estudossts.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudossts.curso.entities.Users;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<Users> findAll(){
+		
+		Users u = new Users(1L, "maria", "maria@gmail.com", "555-555", "12345");
+		return ResponseEntity.ok().body(u);
+		
+	}
+}
